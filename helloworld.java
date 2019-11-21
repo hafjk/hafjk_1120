d2210a46ae038e8e6284ebd48f8e563d675ad605
glb_hafjk <glb_hafjk@qq.com>
2019-11-21 08:48:18 +0800

    helloword02

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+	public static void main(String args[]){
+		System.out.printlf("hello world!");
+		System.out.printlf("111111");
+		System.out.printlf("hello world!");
+		System.out.printlf("222222");
+		
+		
+	}
+	
+}
