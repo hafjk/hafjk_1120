d4e6179abf224e92ff61330743d6129a3c6efeed
glb_hafjk <glb_hafjk@qq.com>
2019-12-04 23:46:50 +0800

    hafjk java commit

---
+++ helloworld 02.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+	public static void main(String args[]){
+		System.out.printlf("hello world!");
+		System.out.printlf("1");
+		System.out.printlf("hello world!");
+		System.out.printlf("2");
+		
+		
+	}
+	
+}
